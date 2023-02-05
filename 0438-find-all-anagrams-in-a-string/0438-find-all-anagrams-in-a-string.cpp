caf20adf7b4a5a33d06eb3ec57046b96547a23fa
ShivamYadav0 <117967079+ShivamYadav0@users.noreply.github.com>
2023-02-05 16:43:02 +0530

    Time: 33 ms (35.34%), Space: 8.8 MB (44.04%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -5,6 +5,7 @@ public:
         if(s1.length()>s2.length())return {};
         unordered_map<char,int>mp;
         unordered_map<char,int>np;
+        int n=s1.length();
         vector<int>v;
         for(auto x:s1)mp[x]++;
         int j=0,k=0,i=0;
@@ -12,7 +13,7 @@ public:
         for(;i<s1.length();i++){np[s2[i]]++;}
         bool f=1;
         for(auto [x,y]:mp)if(y!=np[x]){f=0;break;}
-        if(f)v.push_back(i-s1.length());
+        if(f)v.push_back(i-n);
         for(;i<s2.length();i++){
                      
             np[s2[k]]--;
@@ -20,7 +21,7 @@ public:
             k++;    
            f=1;   
           for(auto [x,y]:mp)if(y!=np[x]){f=0;break;}
-          if(f)v.push_back(i-s1.length()+1);
+          if(f)v.push_back(i-n+1);
         }
         
         return v;
