f2ae3d254773fe8d017aa6c9442de1e397963004
ShivamYadav0 <117967079+ShivamYadav0@users.noreply.github.com>
2023-02-06 17:21:40 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (6.56%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<string>ans;
+    unordered_map<string,int>mp;
+    void rec(string &s,int i,string t,bool f){
+        if(i==s.length()){
+            if(f)
+            ans.push_back(t);
+            return;
+        }
+        t+=s[i];
+        int k=t.length();
+        string v;
+        while(k>0&&t[k-1]!=' '){
+            v+=t[--k];
+        }
+        reverse(v.begin(),v.end());
+        if(mp[v]>0){
+       // char ch=(i<s.length()?' ':);
+        rec(s,i+1,t+(i+1<s.length()?" ":""),1);
+        }
+        rec(s,i+1,t,0);
+    }
+    vector<string> wordBreak(string s, vector<string>& wordDict) {
+        for(auto x:wordDict)mp[x]++;
+        rec(s,0,"",0);
+        return ans;
+    }
+};
