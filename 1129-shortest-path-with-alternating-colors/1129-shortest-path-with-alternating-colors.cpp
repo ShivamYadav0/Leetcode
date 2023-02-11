279b9bd413cec6ebe7921f58d1c70a11f290c3c0
ShivamYadav0 <117967079+ShivamYadav0@users.noreply.github.com>
2023-02-11 20:19:54 +0530

    Time: 20 ms (76.34%), Space: 15.4 MB (51.66%) - LeetHub

---
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
+        
+        vector<pair<int,int>>g[n];
+        for(auto x:redEdges){
+            g[x[0]].push_back({x[1],0});
+        }
+        for(auto x:blueEdges){
+            g[x[0]].push_back({x[1],1});
+        }
+       /* for(int i=0;i<n;i++){
+            cout<<i<<"-";
+            for(auto x:g[i])
+                cout<<x.first<<","<<x.second<<' ';
+            cout<<endl;
+        }
+        cout<<endl;*/
+        queue<vector<int>>q;
+        q.push({0,-1,0});
+        vector<int>dis(n,1e5);
+        vector<int>vis(n,1e5);
+        dis[0]=0;
+        vis[0]=0;
+        while(!q.empty()){
+            int m=q.front()[0];
+            int f=q.front()[1];
+            int c=q.front()[2];
+            q.pop();
+            for(auto x:g[m]){
+                if(f!=x.second&&
+ ((x.second==0&&dis[m]+1<vis[x.first])||(x.second==1&&vis[m]+1<dis[x.first]))){
+                    if(x.second==0)
+                        vis[x.first]=dis[m]+1;
+                    else
+                        dis[x.first]=vis[m]+1;
+            
+                    q.push({x.first,x.second,c+1});
+                }
+            }
+        }
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
+            ans[i]=min(dis[i],vis[i]);
+        }
+            
+        for(auto &x:ans)if(x==1e5)x=-1;
+        
+        return ans;
+    }
+};
