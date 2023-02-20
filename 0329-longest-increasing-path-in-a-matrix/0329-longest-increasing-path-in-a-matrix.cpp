72b49f269be848cb62cae37bb87a40b57b424d62
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2023-02-20 19:58:27 +0530

    Time: 38 ms (97.32%), Space: 16 MB (54.82%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.cpp
@@ -3,17 +3,14 @@ public:
     vector<vector<int>>dp;
     int rec(vector<vector<int>>&v,int i,int j,int p){
       
-        if(dp[i][j]!=-1){
-           // cout<<i<<j<<dp[i][j]<<endl;
+        if(dp[i][j]!=-1)
             return dp[i][j];
-        }
+        
         
         int mx=1;
-        if(i+1<v.size()&&p<v[i+1][j]){
-             mx=max(mx,rec(v,i+1,j,v[i+1][j])+1);
-            //if(v[i][j]==0)
-           // cout<<mx<<endl;
-        }
+        if(i+1<v.size()&&p<v[i+1][j])
+             mx=rec(v,i+1,j,v[i+1][j])+1;
+        
         
         if(i>0&&p<v[i-1][j])
          mx=max(mx, rec(v,i-1,j,v[i-1][j])+1);
@@ -21,8 +18,7 @@ public:
          mx=max(mx, rec(v,i,j+1,v[i][j+1])+1);
         if(j>0&&p<v[i][j-1])
          mx= max(mx,rec(v,i,j-1,v[i][j-1])+1);
-       // if(v[i][j]==0)
-       // cout<<mx<<endl;
+     
         return dp[i][j]=mx;
     }
     int longestIncreasingPath(vector<vector<int>>& matrix) {
@@ -34,12 +30,7 @@ public:
                 mx=max(mx,rec(matrix,i,j,matrix[i][j]));
             }
         }
-      /*  for(auto x:dp){
-            for(auto y:x)
-                cout<<y<<" ";
-            cout<<endl;
-        }
-        cout<<endl;*/
+
         return mx;
     }
 };
