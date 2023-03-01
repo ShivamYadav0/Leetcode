0e8e239b146d7d0bdea52eb92c481a2311b148f3
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2023-03-01 21:29:30 +0530

    Time: 127 ms (91.01%), Space: 61.1 MB (65.89%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.cpp
+++ 0912-sort-an-array/0912-sort-an-array.cpp
@@ -1,41 +1,10 @@
 class Solution {
 public:
     
-    void merge(vector<int>& v,int i,int m,int j){
-        //cout<<i<<" "<<j<<endl;
-        vector<int>t;
-        int x=i,y=m+1;
-        while(x<=m&&y<=j){
-            if(v[x]<=v[y]){
-                t.push_back(v[x++]);
-            }
-            else
-                t.push_back(v[y++]);
-        }
-         while(x<=m){
-            
-                t.push_back(v[x++]);
-        }
-         while(y<=j){
-            
-                t.push_back(v[y++]);
-        }
-        for(int k=0;k<t.size();k++){
-            v[i+k]=t[k];
-        }
-        
-    }
-    void mergesort (vector<int>&v,int i,int j){
-        
-        if(i>=j)return;
-        int m=(i+j)/2;
-        mergesort(v,i,m);
-        mergesort(v,m+1,j);
-        merge(v,i,m,j);
-    }
+    
     vector<int> sortArray(vector<int>& nums) {
         
-        mergesort(nums,0,nums.size()-1);
+       sort(nums.begin(),nums.end());
         return nums;
         
     }
