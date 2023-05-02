187576ef322c18a3efa26779c2132f899e42006d
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2023-05-02 22:42:54 +0530

    Time: 92 ms (54.55%), Space: 19.7 MB (79.36%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -32,17 +32,30 @@ public:
         for(int i=0;i<n;i++)
            if(matrix[i][0]=='1')
             dp[i][0]=1,mx=1;
-        
+        int dpp[m],u[m];
+        for(int i=0;i<m;i++){
+             dpp[i]=0,u[i]=0;
+            if(matrix[0][i]=='1')
+                u[i]=1;
+        }
+           
         for(int i=1;i<n;i++){
+           dpp[0]=(matrix[i][0]=='1');
             for(int j=1;j<m;j++){
               int pv=min(row[i][j-1],col[i-1][j]);
                 if(matrix[i][j]=='1')
-                dp[i][j]=min(dp[i-1][j-1],pv)+1;
-                mx=max(mx,dp[i][j]*dp[i][j]);  
-                //cout<<dp[i][j]<<" ";
+                dpp[j]=min(u[j-1],pv)+1;
+                else
+                    dpp[j]=0;
+                mx=max(mx,dpp[j]*dpp[j]);  
+              //  cout<<dpp[j]<<" ";
                 
             }
-           // cout<<endl;
+             
+            for(int j=0;j<m;j++)
+                u[j]=dpp[j];
+             //u[0]=matrix[i][0]=='1';
+            //cout<<endl;
         }
        // cout<<endl;
         return mx;
