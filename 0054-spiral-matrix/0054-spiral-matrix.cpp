663870d4e8217d969fa8653582a83afcb9b389fd
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2023-05-09 20:32:39 +0530

    Time: 5 ms (33.67%), Space: 6.8 MB (91.37%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -2,28 +2,32 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int sr=0 , er=matrix.size()-1 , sc=0 , ec=matrix[0].size()-1 ;
-        vector<int>ans;
+        vector<int>ans((er+1)*(1+ec));
+                       int c=0;
         while(sr<=er&&sc<=ec){
             for(int i=sc;i<=ec;i++){
-                ans.push_back(matrix[sr][i]);
+                ans[c++]=matrix[sr][i];
             }
+           
+            sr++;
+             if(sr>er||sc>ec)
+                break;
+            for(int i=sr;i<=er;i++){
+                 ans[c++]=(matrix[i][ec]);
+            }
+            
+            ec--;
+             if(sr>er||sc>ec)
+                break;
+            for(int i=ec;i>=sc;i--){
+                ans[c++]=(matrix[er][i]);
+            }
+             
+            er--;
             if(sr>er||sc>ec)
                 break;
-            sr++;
-            for(int i=sr;i<=er;i++){
-                 ans.push_back(matrix[i][ec]);
-            }
-             if(sr>er||sc>ec)
-                break;
-            ec--;
-            for(int i=ec;i>=sc;i--){
-                 ans.push_back(matrix[er][i]);
-            }
-             if(sr>er||sc>ec)
-                break;
-            er--;
             for(int i=er;i>=sr;i--){
-                 ans.push_back(matrix[i][sc]);
+                ans[c++]=(matrix[i][sc]);
             }
             sc++;
             
