7cbdbfac6575e54fe595f03ca94e19b498bcb0af
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2023-05-09 20:30:05 +0530

    Time: 4 ms (33.67%), Space: 6.8 MB (91.37%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -4,19 +4,25 @@ public:
         int sr=0 , er=matrix.size()-1 , sc=0 , ec=matrix[0].size()-1 ;
         vector<int>ans;
         while(sr<=er&&sc<=ec){
-            for(int i=sc;sr<=er&&i<=ec;i++){
+            for(int i=sc;i<=ec;i++){
                 ans.push_back(matrix[sr][i]);
             }
+            if(sr>er||sc>ec)
+                break;
             sr++;
-             for(int i=sr;sc<=ec&&i<=er;i++){
+            for(int i=sr;i<=er;i++){
                  ans.push_back(matrix[i][ec]);
             }
+             if(sr>er||sc>ec)
+                break;
             ec--;
-            for(int i=ec;sr<=er&&i>=sc;i--){
+            for(int i=ec;i>=sc;i--){
                  ans.push_back(matrix[er][i]);
             }
+             if(sr>er||sc>ec)
+                break;
             er--;
-            for(int i=er;sc<=ec&&i>=sr;i--){
+            for(int i=er;i>=sr;i--){
                  ans.push_back(matrix[i][sc]);
             }
             sc++;
