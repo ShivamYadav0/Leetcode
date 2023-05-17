67e7b840278b9dd82a63e22f48b493767ab1346c
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2023-05-17 17:57:50 +0530

    Time: 7 ms (91.04%), Space: 8.9 MB (23.28%) - LeetHub

---
+++ 1409-queries-on-a-permutation-with-key/1409-queries-on-a-permutation-with-key.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    vector<int>bit;
+    void update(int id , int vl){
+        for(int i=id;i<bit.size();i+=(i&(-i))){
+            bit[i]+=vl;
+        }
+    }
+    int query(int id ){
+        int s=0;
+        for(int i=id;i>0;i-=(i&(-i))){
+            s+=bit[i];
+        }
+        return s;
+    }
+    vector<int> processQueries(vector<int>& queries, int m) {
+        bit.resize(2*m+2,0);
+        int n=m;
+        for(int i=1;i<=n;i++)
+            update(i,1);
+        unordered_map<int,int>mp;
+        for(int i=1;i<=m;i++)
+            mp[i]=n+i;
+        //int c=4;
+        //cout<<mp[c]-query(n+c)-1;
+        vector<int>ans;
+        for(auto x:queries){
+            int t=mp[x];
+            int id=t-query(t)-1;
+            update(t,1);
+            mp[x]=n;
+            update(n,-1);
+            n--;
+            ans.push_back(id);
+        }
+        return ans;
+    }
+};
