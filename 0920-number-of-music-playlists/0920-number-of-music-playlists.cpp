aeab410fd32e14963db62e83b91b827901f81cfc
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2023-08-06 22:43:26 +0530

    Time: 365 ms (5.40%), Space: 35.7 MB (5.40%) - LeetHub

--- 0920-number-of-music-playlists/0920-number-of-music-playlists.cpp
+++ 0920-number-of-music-playlists/0920-number-of-music-playlists.cpp
@@ -1,53 +1,48 @@
-
-long long md=1e9+7;
 class Solution {
 public:
-  vector<vector<long long>>dp;
-    
- long long rec(int g,int k,int pi,int n, vector<int>&v,unordered_map<int,int>&mp){
-     if(g==0) {
-        // cout<<mp.size()<<endl;
-         return mp.size()==n;
-       }
-     
-     
-     if(dp[g][mp.size()]!=-1)
-         return dp[g][mp.size()];
-   if(pi!=n)
-    for(int i=0;i<n;i++)
-       if(i!=pi)v[i]--;
-   long long ans=0;
-   for(int i=0;i<n;i++){
-       
-     if(v[i]<=0){
-         int pv=v[i];
-        v[i]=k;
-         mp[i]++;
-        ans+=rec(g-1,k,i,n,v,mp)%md;
-         ans%=md;
-         mp[i]--;
-         if(mp[i]==0)
-             mp.erase(i);
-         //ans++;
-        v[i]=pv;
-         
-      }
-       
+    unordered_map<int,int>mp;
+    vector<vector<int>>dp;
+    int md=1e9+7;
+    int rec(int g,int n,int k){
+        
+        if(g==0 ){
+          
+            return mp.size()==n;
+        }
+        if(dp[g][mp.size()]!=-1){
+            return dp[g][mp.size()];
+        }
+        
+        for(int j=1;j<=n;j++){
+            if(mp.find(j)!=mp.end())
+            mp[j]--;
+        }
+        
+        long long ans=0;
+        for(int j=1;j<=n;j++){
+            if(mp.find(j)==mp.end()|| mp[j]<=0 ){
+                int t=INT_MIN;
+               if(mp.find(j)!=mp.end())
+                   t=mp[j];
+                mp[j]=k+1;
+                ans+=rec(g-1,n,k)%md;
+                if(t==INT_MIN)
+                    mp.erase(j);
+                else
+                    mp[j]=t;
+            }
+        }
+        
+        for(int j=1;j<=n;j++){
+            if(mp.find(j)!=mp.end())
+            mp[j]++;
+        }
+        return dp[g][mp.size()]=ans%md;
     }
-     if(pi!=n)
-
-    for(int i=0;i<n;i++)
-
-       if(i!=pi)v[i]++;
-     
-     return dp[g][mp.size()]=ans;
-     
-  }
     int numMusicPlaylists(int n, int goal, int k) {
-       vector<int>v(n,0);
-       dp.resize(goal+2, vector<long long>(n+2,-1));
-        unordered_map<int,int>mp;
-       // cout<<endl;
-      return  rec(goal,k,n,n,v,mp);
+        
+       dp.resize(goal+1,vector<int>(n+1,-1));
+       
+        return rec(goal,n,k);
     }
 };
