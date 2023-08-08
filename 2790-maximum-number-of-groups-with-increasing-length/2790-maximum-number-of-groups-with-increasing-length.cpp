5d47e24f24aaf48c18930f167e4c4870edb1b1c0
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2023-08-08 19:15:35 +0530

    Time: 176 ms (84.88%), Space: 123.8 MB (23.49%) - LeetHub

--- 2790-maximum-number-of-groups-with-increasing-length/2790-maximum-number-of-groups-with-increasing-length.cpp
+++ 2790-maximum-number-of-groups-with-increasing-length/2790-maximum-number-of-groups-with-increasing-length.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
-    int maxIncreasingGroups(vector<int>& A) {
-        int n = A.size();
-        unordered_map<int, int> count;
-        for (int a : A)
-            count[min(a, n)]++;
-        long long total = 0, k = 0;
-        for (int i = 0; i <= n; i++)
-            for (int j = 0; j < count[i]; j++)
-                if ((total += i) >= (k + 1) * (k + 2) / 2)
-                    k++;
-        return k;
+     bool satisfy(int groups, vector<int> &usageLimits) {
+        int gap = 0;
+        int h = groups;
+        for (int i = 0; i < usageLimits.size(); ++i) {
+            gap = max(h - usageLimits[i] + gap, 0);
+            if (h) h -= 1;
+        }
+        return gap == 0;
+    }
+    int maxIncreasingGroups(vector<int>& usageLimits) {
+        int n = usageLimits.size();
+        sort(usageLimits.begin(), usageLimits.end(), greater<int>());
+        int l = 1, r = n;
+        int res = 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (satisfy(mid, usageLimits)) {
+                res = max(res, mid);
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+        }
+        return res;
+        
     }
 };
