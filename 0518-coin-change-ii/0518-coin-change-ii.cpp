f6a31608014c49db438e6befd83b3f416580f61d
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2023-08-11 10:02:50 +0530

    Time: 16 ms (70.32%), Space: 7.2 MB (90.15%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -8,7 +8,7 @@ class Solution
 
             vector<int> dp(amount + 1, 0);
             dp[0] = 1;
-            int ans = 0;
+           
             for (int i = 0; i < coins.size(); i++)
             {
                 for (int j = 1; j <= amount; j++)
