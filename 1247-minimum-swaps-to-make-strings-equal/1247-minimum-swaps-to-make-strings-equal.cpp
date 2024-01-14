6499204b20f823b3a3e280b8e55aa762effe172a
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2024-01-14 19:53:46 +0530

    Time: 3 ms (43.56%), Space: 6.8 MB (20.89%) - LeetHub

---
+++ 1247-minimum-swaps-to-make-strings-equal/1247-minimum-swaps-to-make-strings-equal.cpp
@@ -0,0 +1,35 @@
+class Solution {
+  public:
+
+    int minimumSwap(string s1, string s2) {
+      int x1 = 0, x2 = 0;
+      for (auto x: s1) x1 += (x == 'x');
+      for (auto x: s2) x2 += (x == 'x');
+
+      int count = 0;
+      if ((x1 + x2) % 2 != 0) return -1;
+      for (int i = 0; i < s1.length(); i++) {
+        int indexToSwap = -1;
+        
+        if (s1[i] != s2[i]) {
+          for (int j = i + 1; j < s1.length(); j++) {
+            if (s1[i] == s1[j]) {
+              indexToSwap = j;
+              if (s1[j] != s2[j]) break;
+            }
+
+          }
+          if (indexToSwap == -1) {
+            swap(s1[i], s2[i]);
+            i--;
+          } else
+            swap(s2[i], s1[indexToSwap]);
+          count++;
+
+        }
+      }
+      return count;
+
+    }
+
+};
