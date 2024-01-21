2cd51b5d3872217b75836bce77aa7964df6e0db2
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2024-01-21 18:07:59 +0530

    Time: 362 ms (100.00%), Space: 137.4 MB (85.71%) - LeetHub

--- 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.cpp
+++ 3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.cpp
@@ -2,8 +2,15 @@ class Solution {
  public:
 
   long long minimumCost(vector<int>& nums, int k, int dist) {
-    set<pair<int, int>> ms;
+    // first element will be included always as 1st segment starts from it
+      
     long long s = nums[0];
+    
+    // now b/w second and last segment diff will be at most dist
+    // so now question becomes sliding window type 
+    // optimal solution is to choose min sum k-1 elements across the windows 
+    // now tc accetable is o(n*logn) and we have choose first k-1 elements from window and add elemts into window such that their order remains same so after a lot of brainstorming set is best option
+    set<pair<int, int>> ms;
     k--;
     for (int i = 1; i <= dist + 1; i++) {
       ms.insert({nums[i], i});
