d74cacf1398ce94b61dde918cad2ce04bedcf49d
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2024-02-03 22:21:41 +0530

    Time: 15 ms (52.94%), Space: 10.9 MB (23.07%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.cpp
@@ -1,25 +1,38 @@
 class Solution {
 public:
-    vector<vector<int>>dp;
+
     vector<int>dpp;
-    int rec(vector<int>&arr,int i,int p,int k){
-        if(i==arr.size()){
-             return 0;
+    int rec(vector<int>&arr, int i, int k) {
+        //base case if we have covered whole array so return 0 ;
+        if (i == arr.size()) {
+//Note:- we are returning 0 not the calculated sum as we are already doing that in recursive call.
+            return 0;
         }
-        if(dpp[i]!=-1)return dpp[i];
-        int ans=0;
-       
-         int mx=arr[i];
-        for(int j=i;j<arr.size()&&j<i+k;j++){
-            mx=max(mx,arr[j]);
-             int l=rec(arr,j+1,i,k)+(j-i+1)*mx;
-            ans=max(ans,l);
+        //returning memoised value
+        if (dpp[i] != -1)return dpp[i];
+        // ans variable to keep track of max state among all
+        int ans = 0;
+
+        //mx variable to store the max element of subsegment
+        int mx = arr[i];
+        // traversing from i to i+k as we can have at most k elements in subsegment
+        // so we will try to find max sum among all state i.e from index i to min(i+k,arr.size()-1)
+        for (int j = i; j < arr.size() && j < i + k; j++) {
+            //keeping track of max element
+            mx = max(mx, arr[j]);
+            //calculating current segment answer by adding mx (j-i+1) times i.e for each state and then adding answer from segment j+1 as current ends at j.
+            int l = (j - i + 1) * mx + rec(arr, j + 1, k);
+
+            //finding max among all states
+            ans = max(ans, l);
         }
-        return dpp[i]= ans;
+        //storing answer and returning it
+        return dpp[i] = ans;
     }
     int maxSumAfterPartitioning(vector<int>& arr, int k) {
-        dp.resize(arr.size()+1,vector<int>(k+1,-1));
-        dpp.resize(arr.size()+1,-1);
-        return rec(arr,0,-1,k);
+        //initialising dp
+        dpp.resize(arr.size() + 1, -1);
+        //calling rec function
+        return rec(arr, 0, k);
     }
 };
