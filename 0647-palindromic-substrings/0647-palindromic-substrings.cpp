2e8e23e89a8807e364e81a5e56403e737f5b7dab
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2024-02-10 16:21:06 +0530

    Time: 21 ms (30.21%), Space: 9.4 MB (57.80%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -8,28 +8,26 @@ public:
         
         for(int i=0;i<n;i++){
              dp[i][i]=1;
-            ans++;
+           
         }
            
         for(int i=0;i<n-1;i++){
-            if(s[i]==s[i+1]){
-                  dp[i][i+1]=1;
-                ans++;
-            }
+           
+                  dp[i][i+1]=(s[i]==s[i+1]);
+              
               
         }
         for(int i=2;i<n;i++){
             for(int j=0;j<n-i;j++){
-                if(s[j]==s[j+i]&&dp[j+1][i+j-1]){
-                    dp[j][j+i]=1;
-                    ans++;
-                }
+              
+                    dp[j][j+i]=(s[j]==s[j+i]&&dp[j+1][i+j-1]);
+                    
             }
         }
         ans=0;
          for(int i=0;i<n;i++){
             for(int j=0;j<n-i;j++){
-                if(dp[j][j+i])ans++;
+                ans+=(dp[j][j+i]);
             }
          }
         return ans;
