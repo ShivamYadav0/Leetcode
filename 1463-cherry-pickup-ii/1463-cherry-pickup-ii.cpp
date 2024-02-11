54b4f831b6f87b0680dbbef2a737783d474c716e
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2024-02-11 21:29:17 +0530

    Time: 564 ms (5.10%), Space: 124.2 MB (5.03%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -2,51 +2,51 @@ class Solution {
 public:
     vector<vector<int>>gr;
     int n, m;
-    int dp[70][70][70][70];
+    int dp[70][70][70];
     int dir[3][2] = {{1, 1}, {1, 0}, { 1, -1}};
     bool isValid(int x, int y) {
         if (x < 0 || y < 0 || x >= n || y >= m)return false;
         return true;
     }
-    int rec(int i1, int j1, int i2, int j2) {
+    int rec(int i, int j1, int j2) {
 
-        if(dp[i1][j1][i2][j2]!=-1){
-            return dp[i1][j1][i2][j2];
+        if (dp[i][j1][j2] != -1) {
+            return dp[i][j1][j2];
         }
         int vl = 0;
-        if (i1 == i2 && j1 == j2) {
-            vl = gr[i1][j1];
+        if (j1 == j2) {
+            vl = gr[i][j1];
         }
         else {
-            vl = gr[i1][j1] + gr[i2][j2];
+            vl = gr[i][j1] + gr[i][j2];
         }
-       
-        int ans = 0;
-        vector<int>calls={0};
+
+        int ans = vl;
+        vector<int>calls;
         for (auto di1 : dir) {
-            int ni1 = i1 + di1[0];
+            int ni1 = i + 1;
             int nj1 = j1 + di1[1];
             if (isValid(ni1, nj1)) {
                 for (auto di2 : dir) {
-                    int ni2 = i2 + di2[0];
+
                     int nj2 = j2 + di2[1];
-                    if (isValid(ni2, nj2)) {
-                        calls.push_back(rec(ni1, nj1, ni2, nj2));
+                    if (isValid(ni1, nj2)) {
+                        calls.push_back(rec(ni1, nj1, nj2));
                     }
                 }
 
             }
         }
         for (auto x : calls) {
-            ans = max(ans, x+vl);
+            ans = max(ans, x + vl);
         }
-        return  dp[i1][j1][i2][j2] = ans;
+        return  dp[i][j1][j2] = ans;
     }
     int cherryPickup(vector<vector<int>>& grid) {
         gr = grid;
         n = gr.size();
         m = gr[0].size();
         memset(dp, -1, sizeof(dp));
-        return  rec(0,0,0,m-1);
+        return  rec(0, 0, m - 1);
     }
 };
