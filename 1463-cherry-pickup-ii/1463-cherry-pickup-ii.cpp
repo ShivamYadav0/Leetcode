75d5986d9e72973080f654af05597986c1f2b791
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2024-02-11 21:34:49 +0530

    Time: 30 ms (97.34%), Space: 12.3 MB (80.55%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    
+
     int n, m;
     int dp[70][70][70];
     int dir[3][2] = {{1, 1}, {1, 0}, { 1, -1}};
@@ -8,7 +8,7 @@ public:
         if (x < 0 || y < 0 || x >= n || y >= m)return false;
         return true;
     }
-    int rec(vector<vector<int>>&gr,int i, int j1, int j2) {
+    int rec(vector<vector<int>>&gr, int i, int j1, int j2) {
 
         if (dp[i][j1][j2] != -1) {
             return dp[i][j1][j2];
@@ -24,27 +24,27 @@ public:
         int ans = vl;
         vector<int>calls;
         for (auto& di1 : dir) {
-            int ni1 = i + 1;
+            int ni = i + 1;
             int nj1 = j1 + di1[1];
-            if (isValid(ni1, nj1)) {
+            if (isValid(ni, nj1)) {
                 for (auto &di2 : dir) {
 
                     int nj2 = j2 + di2[1];
-                    if (isValid(ni1, nj2)) {
-                         ans = max(ans,rec(gr,ni1, nj1, nj2)+vl);
+                    if (isValid(ni, nj2)) {
+                        ans = max(ans, rec(gr, ni, nj1, nj2) + vl);
                     }
                 }
 
             }
         }
-      
+
         return  dp[i][j1][j2] = ans;
     }
     int cherryPickup(vector<vector<int>>& gr) {
-      
+
         n = gr.size();
         m = gr[0].size();
         memset(dp, -1, sizeof(dp));
-        return  rec(gr,0, 0, m - 1);
+        return  rec(gr, 0, 0, m - 1);
     }
 };
