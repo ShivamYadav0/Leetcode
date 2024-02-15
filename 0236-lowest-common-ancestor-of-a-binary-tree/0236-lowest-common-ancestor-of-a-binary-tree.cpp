4699dfb76c77a1bfbc22e42d8c841d835e7c8e04
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2024-02-15 10:44:04 +0530

    Time: 17 ms (32.43%), Space: 16 MB (42.83%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -11,30 +11,29 @@ class Solution {
 public:
     int lim=2;
     TreeNode *lca;
-    vector<bool> rec(TreeNode *rt,TreeNode *p,TreeNode *q){
+    TreeNode* rec(TreeNode *rt,TreeNode *p,TreeNode *q){
         if(!rt){
-            vector<bool>emp(lim,0);
-            return emp;
+           return NULL;
         }
-        vector<bool>a=rec(rt->left,p,q);
-        vector<bool>b=rec(rt->right,p,q);
-        vector<bool>c=a;
-        for(int i=0;i<lim;i++){
-            c[i]=(a[i]||b[i]);
+        TreeNode *a=rec(rt->left,p,q);
+        TreeNode *b=rec(rt->right,p,q);
+       
+       if(rt==p){
+           if(!a)
+           a=rt;
+           else if(!b)b=rt;
+       }
+     if(rt==q){
+               if(!a)
+           a=rt;
+           else if(!b)b=rt;
         }
-        if(p==rt){
-            c[0]=1;
+        if(a||b){
+            if(!lca&&a&&b)
+            lca=rt;
+            return rt;
         }
-        else if(q==rt){
-            c[1]=1;
-        }
-         for(int i=0;i<lim&&!lca;i++){
-            if(!c[i])break;
-             if(i==lim-1){
-                 lca=rt;
-             }
-        }
-        return c;
+        return NULL;
     }
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
         lca=NULL;
