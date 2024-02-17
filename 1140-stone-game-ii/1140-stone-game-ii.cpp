7408eebdc89f913bf3b31abef6271474836d41b1
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2024-02-17 14:27:12 +0530

    Time: 8 ms (89.48%), Space: 9.8 MB (86.46%) - LeetHub

---
+++ 1140-stone-game-ii/1140-stone-game-ii.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int dp[101][101];
+    int rec(int i,int m,vector<int>&p){
+        if(i>=p.size())return 0;
+        if(dp[i][m]!=-1)return dp[i][m];
+        int lt=i+2*m;
+        if(lt>=p.size())lt=p.size();
+        int ans=-1e7;
+        int cs=0;
+        int ts=0;
+        for(int j=i;j<p.size();j++)ts+=p[j];
+        for(int j=i;j<lt;j++){
+            //ts+=p[j];
+            cs+=p[j];
+            int rans=rec(j+1,max(m,j-i+1),p);
+            //cout<<rans+cs<<" ";
+            ans=max(ans,cs+rans);
+            
+        }
+        return dp[i][m]=ts-ans;
+    }
+    int stoneGameII(vector<int>& piles) {
+      memset(dp,-1,sizeof(dp));
+        int ts=0;
+        for(auto x:piles)ts+=x;
+        return ts-rec(0,1,piles);
+    }
+};
