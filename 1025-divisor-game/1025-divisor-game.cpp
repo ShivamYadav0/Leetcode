54c7b7a81673a2c4eb6f1e424aa8e96201611e24
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2024-02-17 14:04:55 +0530

    Time: 5 ms (10.09%), Space: 7 MB (34.44%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,7 +1,19 @@
 class Solution {
 public:
-    
+    int dp[1001];
+    bool check(int n){
+        if(n==1)return false;
+        if(dp[n]!=-1)return dp[n];
+        for(int j=1;j<n;j++){
+            if(n%j==0){
+                if(!check(n-j))
+                    return dp[n]=true;
+            }
+        }
+        return dp[n]=false;
+    }
     bool divisorGame(int n) {
-        return n%2==0;          
+        memset(dp,-1,sizeof(dp));
+        return check(n);          
     } 
 };
