aebe85c9c37cfe21aa4737d812afec5a14f701f0
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2024-02-17 16:03:20 +0530

    Time: 154 ms (81.81%), Space: 140.1 MB (38.08%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.cpp
+++ 1406-stone-game-iii/1406-stone-game-iii.cpp
@@ -1,35 +1,35 @@
 class Solution {
 public:
-     vector<int>ss;
-     vector<vector<int>>dpp;
+     
      vector<int>dp;
     int limit=-1e8;
-    int rec(int i,vector<int>&v){
+    int rec(int i,vector<int>&v,int ts){
          if(i==v.size())return 0;
         //int ans=0;
         if(dp[i]!=limit)return dp[i];
         int lt=i+3;
         if(lt>=v.size())lt=v.size();
-        int s=0;
+       // int s=0;
         int ans=-limit;
         for(int j=i;j<lt;j++){
-            s+=v[j];
-            ans=min(ans,rec(j+1,v));
+            ts-=v[j];
+            ans=min(ans,rec(j+1,v,ts));
+            ts+=v[j];
         }
-        return dp[i]=ss[i]-ans;
+        return dp[i]=ts-ans;
     }
     string stoneGameIII(vector<int>& st) {
-        ss=st;
-        //dp.resize(st.size()+1,vector<int>(4,-1));
+       
         dp.resize(st.size()+1,limit);
-       for(int i=st.size()-2;i>=0;i--){
-           ss[i]=ss[i+1]+st[i];
+        int ts=0;
+       for(int i=st.size()-1;i>=0;i--){
+          ts+=st[i];
        }
-        int ans=rec(0,st);
-        if(ans*2>ss[0]){
+        int ans=rec(0,st,ts);
+        if(ans*2>ts){
             return "Alice";
         }
-        if(ans*2<ss[0])return "Bob";
+        if(ans*2<ts)return "Bob";
         return "Tie";
     }
 };
