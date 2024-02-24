68176c2f6bd40f8c6a2c3e27bea6e21752e90c9c
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2024-02-24 18:38:57 +0530

    Time: 433 ms (77.87%), Space: 184.9 MB (44.67%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -4,7 +4,7 @@ struct node {
 class Solution {
 public:
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int fp) {
-      vector<vector<node>>g(n);
+        vector<vector<node>>g(n);
         for (auto x : meetings) {
             g[x[0]].push_back({x[1], x[2]});
             g[x[1]].push_back({x[0], x[2]});
@@ -16,25 +16,23 @@ public:
         dis[fp] = 0;
         q.push({0, 0});
         q.push({fp, 0});
-      while (!q.empty()) {
-
+        while (!q.empty()) {
             auto c = q.front();
             q.pop();
-        
+
             for (auto x : g[c.v]) {
                 if (x.w >= c.w && dis[x.v] > x.w) {
                     q.push({x.v, x.w});
                     dis[x.v] = x.w;
-                
                 }
             }
         }
         vector<int>v;
-        int i = 0;
-        for (auto x : dis) {
-            if (x != lim)
+      
+        for (int i=0;i<n;i++) {
+            if (dis[i] != lim)
                 v.push_back(i);
-            i++;
+          
         }
         return v;
     }
