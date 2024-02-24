dc3601b83a164a2b397d8360290ad118ec448ba8
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2024-02-24 18:37:50 +0530

    Time: 418 ms (84.84%), Space: 184.8 MB (45.08%) - LeetHub

--- 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.cpp
@@ -1,45 +1,38 @@
-
-struct node{
-     int v,w;
+struct node {
+    int v, w;
 };
 class Solution {
 public:
     vector<int> findAllPeople(int n, vector<vector<int>>& meetings, int fp) {
-        
-        
-        vector<vector<node>>g(n);
-        for(auto x:meetings){
-            g[x[0]].push_back({x[1],x[2]});
-            g[x[1]].push_back({x[0],x[2]});
+      vector<vector<node>>g(n);
+        for (auto x : meetings) {
+            g[x[0]].push_back({x[1], x[2]});
+            g[x[1]].push_back({x[0], x[2]});
         }
         queue<node>q;
-        int lim=INT_MAX;
-         vector<int>dis(n,lim);
-        dis[0]=0;
-        dis[fp]=0;
-        q.push({0,0});
-        q.push({fp,0});
-        //unordered_map<int,int>mp;
-       // mp[0]++;
-       // mp[fp]++;
-       
-        while(!q.empty()){
-            
-            auto c=q.front();
+        int lim = INT_MAX;
+        vector<int>dis(n, lim);
+        dis[0] = 0;
+        dis[fp] = 0;
+        q.push({0, 0});
+        q.push({fp, 0});
+      while (!q.empty()) {
+
+            auto c = q.front();
             q.pop();
-           // mp[c.v]++;
-            for(auto x:g[c.v]){
-                if(x.w>=c.w && dis[x.v]>x.w){
-                    q.push({x.v,x.w});
-                    dis[x.v]=x.w;
-                   // mp[x.v]++;
+        
+            for (auto x : g[c.v]) {
+                if (x.w >= c.w && dis[x.v] > x.w) {
+                    q.push({x.v, x.w});
+                    dis[x.v] = x.w;
+                
                 }
             }
         }
         vector<int>v;
-        int i=0;
-        for(auto x:dis){
-            if(x!=lim)
+        int i = 0;
+        for (auto x : dis) {
+            if (x != lim)
                 v.push_back(i);
             i++;
         }
