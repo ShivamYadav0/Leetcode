5608c1d2b193111fcc9543399b5fc08d41accc27
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2024-02-25 21:24:03 +0530

    Time: 732 ms (50.90%), Space: 212.2 MB (35.93%) - LeetHub

--- 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.cpp
+++ 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.cpp
@@ -1,46 +1,57 @@
 class Solution {
 public:
-    unordered_map<int, vector<int>> prime2index;
-    unordered_map<int, vector<int>> index2prime;
-    void dfs(int index, vector<int>& visitedIndex, unordered_map<int,bool>& visitedPrime){
-        if(visitedIndex[index] == true) return;
-        visitedIndex[index] = true;
-
-        for(auto &prime : index2prime[index]){
-            if(visitedPrime[prime] == true) 
-                continue;
-            visitedPrime[prime] = true;
-            for(auto &index1 : prime2index[prime]){
-                if(visitedIndex[index1] == true) continue;
-                dfs(index1, visitedIndex, visitedPrime);
+    unordered_map<int,vector<int>>mp1;
+    unordered_map<int,vector<int>>mp2;
+    unordered_map<int,int>vis1;
+    unordered_map<int,int>vis2;
+    void dfs(int id,bool flag){
+        
+       vis1[id]=1;
+        for(auto x:mp1[id]){
+            if(vis2[x]>0)continue;
+            vis2[x]=1;
+            for(auto y:mp2[x]){
+                if(vis1[y]>0)continue;
+                dfs(y,flag);
             }
         }
     }
-
     bool canTraverseAllPairs(vector<int>& nums) {
-        for (int i=0; i<nums.size(); i++) {
-            int temp = nums[i];
-            for (int j = 2; j*j <= nums[i]; j++) {
-                if (temp % j == 0) {
-                    prime2index[j].push_back(i);
-                    index2prime[i].push_back(j);
-                    while (temp % j == 0)
-                      temp /= j;
+        int n=nums.size();
+        sort(nums.begin(),nums.end());
+        
+        if(nums.size()==1)return true;
+        if(nums[0]==1)return false;
+        for(int j=0;j<n;j++){
+            //if(mp[])
+            int x=nums[j];
+            //if(mp2[x].size()>0)continue;
+            for(int i=2;i*i<=x;i++){  
+                if(x%i==0){
+                    
+                    mp1[j].push_back(i);
+                    mp2[i].push_back(j);
+                    while(x>1&&x%i==0)x/=i;
+                   
                 }
             }
-            if (temp > 1) {
-                prime2index[temp].push_back(i);
-                index2prime[i].push_back(temp);
+            if(x>1){
+                 mp2[x].push_back(j);
+                 mp1[j].push_back(x);
             }
         }
-
-        vector<int> visitedIndex(nums.size(),0);
-        unordered_map<int,bool> visitedPrime;
-        dfs(0, visitedIndex, visitedPrime);     
-
-        for(int i=0; i<visitedIndex.size(); i++) 
-            if(visitedIndex[i] == false) 
-                return false;
-        return true;    
+    
+        int src=nums[n-1];
+        dfs(0,0);
+        bool flag=1;
+        int i=0;
+        for(auto x:nums){
+            if(vis1[i++]>0){ 
+            }
+            else 
+            flag=0;
+        }
+        
+        return flag;
     }
 };
