51924bbf4a7f9b9d2fffc79a00006e477ffe83a9
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2024-02-29 23:08:04 +0530

    Time: 1432 ms (5.00%), Space: 175 MB (9.15%) - LeetHub

---
+++ 1609-even-odd-tree/1609-even-odd-tree.cpp
@@ -0,0 +1,36 @@
+class Solution {
+ 
+    unordered_map<int , int > m;
+    
+    bool dfs(TreeNode* root , int level ){
+        
+        if (root == NULL )  return true;
+        
+        if (level & 1){
+            if ( root->val  &  1  )  return false; // if level is odd then value should not be odd
+        }
+        else if ( !(root->val & 1) ) return false; // if level is even then value should not be even
+        
+        
+        if (m.find(level) != m.end() ){
+            if (level & 1 ){
+                if (m[level] <= root->val ) return false; // should be strictely strictly decreasing   
+            }
+            else if (m[level] >= root->val ) return false; // should be strictely strictly increasing  
+            
+        }
+        
+        m[level] =  root->val;
+    
+        return dfs(root->left , level + 1) && dfs(root->right, level +  1);
+    }
+    
+    
+public:
+    bool isEvenOddTree(TreeNode* root) {
+        
+        return dfs(root, 0 );
+        
+        
+    }
+};
