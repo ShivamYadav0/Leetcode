3e9752336fa66a80a4928b71527733cd42faa9c9
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2024-05-14 21:59:37 +0530

    Time: 129 ms (61.58%), Space: 9.1 MB (81.32%) - LeetHub

--- 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -8,25 +8,22 @@ public:
     }
     int rec(vector<vector<int>>& g,int i ,int j,int s=0){
         
-        int p=g[i][j];
-       // s+=p;
-       // mx=max(mx,s);
+       int p=g[i][j];
+        s+=p;
+        mx=max(mx,s);
         g[i][j]=0;
         
         
         int arr[]={1,-1,0,0};
         int arr1[]={0,0,1,-1};
-        bool f=0;
+        
         for(int k=0;k<4;k++){
             if(isValid(g,i+arr[k],j+arr1[k])){
-                f=1;
-                rec(g,i+arr[k],j+arr1[k],s+g[i+arr[k]][j+arr1[k]]);
+                rec(g,i+arr[k],j+arr1[k],s);
             }
         }
-        if(!f){
-            mx=max(mx,s);
-        }
-     
+        
+        
         g[i][j]=p;
         
         return s;
@@ -39,7 +36,7 @@ public:
         for(int i=0;i<grid.size();i++){
             for(int j=0;j<grid[0].size();j++){
                 if(isValid(grid,i,j))
-                rec(grid,i,j,grid[i][j]);
+                rec(grid,i,j);
             }
         }
         return mx;
