ded63534ca082f8be0ec112628a63cb13ac1387d
Shivam Yadav <117967079+ShivamYadav0@users.noreply.github.com>
2024-05-18 21:17:35 +0530

    Time: 8 ms (18.19%), Space: 18.6 MB (6.74%) - LeetHub

---
+++ 0979-distribute-coins-in-binary-tree/0979-distribute-coins-in-binary-tree.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int ans=0;
+    int rec(TreeNode *rt,TreeNode *pr,unordered_map<TreeNode*,int>&mp){
+        if(!rt||mp[rt])return 0;
+      
+       
+        
+        int a=rec(rt->left,rt,mp);
+        int b=rec(rt->right,rt,mp);
+         if((mp[rt->left]&&mp[rt->right])){
+            if(rt->val<1){
+                pr->val+=(rt->val-1);
+                ans+=abs(rt->val-1);
+            }
+            else if(rt->val>1){
+                pr->val+=rt->val-1;
+                ans+=rt->val-1;
+            }
+            mp[rt]++;
+        }
+       
+        return a+b;
+        
+    }
+    int distributeCoins(TreeNode* root) {
+        unordered_map<TreeNode*,int>mp;
+        mp[NULL]++;
+        rec(root,root,mp);
+        return ans;
+    }
+};
